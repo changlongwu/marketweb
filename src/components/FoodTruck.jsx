2308fa973a6c408363b799a810f504647c4ffec9
changlongwu <changlong_wu@berkeley.edu>
2024-05-29 23:49:18 -0700

    use import to get the image

--- src/components/FoodTruck.jsx
+++ src/components/FoodTruck.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import Content from './Content';
-
+import foodTruckImage1 from './img/foodtruck1.jpeg';
+import foodTruckImage2 from './img/foodtruck2.jpeg';
+import foodTruckImage3 from './img/foodtruck3.jpeg';
 const FoodTruck = () =>{
     return (
         <div className='FoodTruck'>
             
             <div className='card-container'>
                 <div className='card-item'>
-                    <Content imgSrc="src/img/foodtruck1.jpeg" TruckName = "Birria-Landia" Country = "Mexican"/>
+                    <Content imgSrc={foodTruckImage1} TruckName = "Birria-Landia" Country = "Mexican"/>
 
                 </div>
 
                 <div className='card-item'>
-                    <img src="src/img/foodtruck2.jpeg" alt="" />
+                    <img src={foodTruckImage2} alt="" />
                     <h4>Mysttik Masala</h4>
                     <h5>Indian</h5>
                     <button>view Menu</button>
                 </div>
                 <div className='card-item'>
-                    <img src="src/img/foodtruck3.jpeg" alt="" />
+                    <img src={foodTruckImage3} alt="" />
                     <h4>Mysttik Masala</h4>
                     <h5>Middle Eastern</h5>
                     <button>view Menu</button>
